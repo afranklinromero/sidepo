528e1d6a7f4806379fdd4af89c2387278216e177
choco <afranklin.romero@gmail.com>
2023-06-09 15:12:19 -0400

    servidor funcionando

--- resources/views/denuncia.blade.php
+++ resources/views/denuncia.blade.php
@@ -3,11 +3,12 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <form action="/denuncia" method="GET">
+        <form action="/denuncia" method="POST">
+        @csrf
             <input type ="text" name ="prueba">
 
             <input type ="submit" value="Enviar" >
-            @csrf
+           
     </div>
 </div>
 @endsection
