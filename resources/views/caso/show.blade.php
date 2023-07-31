d7b961cc6d1fe078bd672dc6edc5864b10f648c0
choco <afranklin.romero@gmail.com>
2023-07-31 16:36:11 -0400

    actualizacion links

--- resources/views/caso/show.blade.php
+++ resources/views/caso/show.blade.php
@@ -188,7 +188,7 @@
                                     
                                     <div style="position: relative; width: 65%; height: 400px; ">
                                     <object data="{{asset('storage/pdf/' . $archivodenuncia->pdf)}}" type="application/pdf" frameborder="0" width="100%" height="600px" style="padding: 20px;">
-                                    <embed src="https://drive.google.com/file/d/1CRFdbp6uBDE-YKJFaqRm4uy9Z4wgMS7H/preview?usp=sharing" width="100%" height="600px"/> 
+                                    <iframe src="https://docs.google.com/viewer?url={{asset('storage/pdf/' . $archivodenuncia->pdf)}}&embedded=true" style="width:100%; height:650px;" frameborder="0"></iframe>
                                     </object>
                                     
 
